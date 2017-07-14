13bf71043a1b7cf0b267138ddfe6d6a8dffe502f
enyeldoc2011@gmail.com <enyeldoc2011@gmail.com>
2017-07-14 19:44:09 -0400

    second commit

---
+++ db/migrate/20170714233715_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name_p
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
