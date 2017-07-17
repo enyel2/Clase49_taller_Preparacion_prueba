f9f5aba3dbd4ec479e999b1af22f93d1727befaf
enyeldoc2011@gmail.com <enyeldoc2011@gmail.com>
2017-07-17 14:49:28 -0400

    se añadio column prece, premium and validate a model product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validate :price, presence: true
+  # rails 4 validates :category_id, prensence: true
+
+  before_save :default_premium 
+
+  def :default_premium
+  	unless self.premium.present? || self.premium == true
+  		self.premium = false
+  	end
+  end
 end
