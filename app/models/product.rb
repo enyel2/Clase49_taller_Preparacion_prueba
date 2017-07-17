94bcd80b9618fc6f7d8b8bc09878298cb1d227e2
enyeldoc2011@gmail.com <enyeldoc2011@gmail.com>
2017-07-17 16:56:38 -0400

    Mejoras callback para premium

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,14 +2,14 @@ class Product < ApplicationRecord
   belongs_to :category
   validates :price, presence: true
   # rails 4 validates :category_id, prensence: true
+  before_save :premium_por_defecto 
 
-  #before_save :default_premium 
+  def premium_por_defecto
+  	unless self.premium.present? || self.premium == true
+  		self.premium = false
+  	end
+  end
 
-  #def :default_premium
-  #	unless self.premium.present? || self.premium == true
-  #		self.premium = false
-  #	end
-  #end
 end
 
 
