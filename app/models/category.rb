d26e49241267f49bda476a4913036b5c37e72f4e
enyeldoc2011@gmail.com <enyeldoc2011@gmail.com>
2017-07-18 16:11:47 -0400

    se añadio la columna discount al model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
 	has_many :products, dependent: :destroy
+
+	validate :descuento_valido
+
+	def descuento_valido
+		if self.discount < 0 || self.discount > 100
+			errors.add(:discount, "El descuento tiene que estar entre 0 - 100")
+		end
+	end	
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,19 +4,34 @@ class Product < ApplicationRecord
   # rails 4 validates :category_id, prensence: true
   before_save :premium_por_defecto 
 
+  after_destroy :ultimo_product
+
   #estoy diciendo mostrar todos los productos premium que son verdaderos
   scope :solo_premium, -> { where(premium: true)}
 
-  scope :last_n, -> (n) {limit(n)}
+  #Muestra  los n producto ingresado
+  scope :last_n, -> (n) { limit(n) }
 
+  #Metodo que por defecto asigna valores false
   def premium_por_defecto
   	unless self.premium.present? || self.premium == true
   		self.premium = false
   	end
   end
 
+  #al borrar el ultimo producto de una categoria borrar la categoria
+  def ultimo_product
+  	cats = Category.all
+  		cats.each do |n|
+  			if n.products.empty?
+  				n.destroy
+  			end
+  		end
+  end
 
-
+  def precio_final
+  	@precio_final = self.price - ((self.price * self.category.discount).to_f / 100)
+  end
 
 end
 
